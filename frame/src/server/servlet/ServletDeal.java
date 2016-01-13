c98c9587b11a74a3ef585459f0dc2feea3333bdd
lijunbo <252500187@qq.com>
2016-01-13 11:27:56 +0800

    first commit

---
+++ frame/src/server/servlet/ServletDeal.java
@@ -0,0 +1,39 @@
+package server.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by lijunbo on 2016/1/12.
+ */
+public class ServletDeal extends HttpServlet {
+
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        super.service(request, response);
+        response.getWriter().write("aaaa");
+        System.out.println(0);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        System.out.println(-1);
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        PrintWriter printWriter = response.getWriter();
+        printWriter.write("hehe");
+        System.out.println(1);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        System.out.println(2);
+    }
+}
---
+++ frame/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lijunbo
+  Date: 2016/1/12
+  Time: 15:38
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+hehe
+  </body>
+</html>
